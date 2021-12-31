614b4702a6d3ec4c5b36f2ea63d20ee204dea090
wenxich <wenxi.c.chen@gmail.com>
2021-12-31 03:10:04 -0500

    Initial commit

---
+++ gpacalc.js
@@ -0,0 +1,34 @@
+function CalculateGPA(classes, totalCredits, resultElementName) {
+    //use Number wrapper on classes entered
+    for(let i = 0; i < classes.length; i++) {
+      classes[i] = Number(classes[i]);
+    }
+    
+    //use Number wrapper on credits entered
+    for(let i = 0; i < totalCredits.length; i++) {
+      totalCredits[i] = Number(totalCredits[i]);
+    }
+    
+    //calculate the average
+    let sum = 0;
+    let sumCredits = 0;
+    
+    //sum up all the credits entered
+    for(let i = 0; i < totalCredits.length; i++) {
+      if(!totalCredits[i].isNaN) {
+        sumCredits += totalCredits[i];
+      }
+    }
+    
+    //update sum and count
+    for(let i = 0; i < classes.length; i++) {
+      if(!classes[i].isNaN) {
+        sum += classes[i]*(totalCredits[i]/sumCredits);
+      }
+    }
+    var average = Math.round((sum + Number.EPSILON) * 100) / 100
+     
+    var resultElement = document.getElementById(resultElementName);
+    
+    resultElement.innerHTML = average;
+  }
